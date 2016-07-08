5792b7bbcc805b6cdc8e8fafd76a11ef3b637b0e
hyounwoo.ko <lj1212.kit@gmail.com>
2016-07-08 18:06:25 +0900

    callback과 promise 병행하도록 로직 수정

--- src/business/common/common-business.ts
+++ src/business/common/common-business.ts
@@ -1,4 +1,7 @@
+'use strict';
+
 import mongoose = require('mongoose');
+import {Promise} from 'es6-promise';
 
 export default class CommonBusiness<T extends mongoose.Document> {
     private _model: mongoose.Model<mongoose.Document>;
@@ -13,53 +16,109 @@ export default class CommonBusiness<T extends mongoose.Document> {
     }
 
     /**
-     * 추가 
+     * 추가 (callback 객체가 없다면 Promise 라고 판단하고 대응한다.)
      * 
      * @param {T} item
      * @param {(error: any, result: any) => void} callback
      */
-    create(item: T, callback: (error: any, result: any) => void) {
-        this._model.create(item, callback);
+    create(item: T, callback: (error: any, result: any) => void): Promise<Object> {
+        if (callback) {
+            this._model.create(item, callback);
+            return;
+        }
+        return new Promise((resolve: any, reject: any) => {
+            this._model.create(item, (err: any, res: mongoose.Document) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
     }
 
     /**
-     * 갱신
+     * 갱신 (callback 객체가 없다면 Promise 라고 판단하고 대응한다.)
      * 
      * @param {mongoose.Types.ObjectId} _id
      * @param {T} item
      * @param {(error: any, result: any) => void} callback
      */
-    update(_id: mongoose.Types.ObjectId, item: T, callback: (error: any, result: any) => void) {
-        this._model.update({ _id: _id }, item, callback);
+    update(_id: mongoose.Types.ObjectId, item: T, callback: (error: any, result: any) => void): Promise<Object> {
+        if (callback) {
+            this._model.update({ _id: _id }, item, callback);
+            return;
+        }
+        return new Promise((resolve: any, reject: any) => {
+            this._model.update({ _id: _id }, item, (err: any, res: mongoose.Document) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
     }
 
     /**
-     * 삭제 
+     * 삭제 (callback 객체가 없다면 Promise 라고 판단하고 대응한다.)
      * 
      * @param {string} _id
      * @param {(error: any, result: any) => void} callback
      */
-    delete(_id: string, callback: (error: any, result: any) => void) {
-        this._model.remove({ _id: this.toObjectId(_id) }, (err) => callback(err, null));
+    delete(_id: string, callback: (error: any, result: any) => void): Promise<Object> {
+        if (callback) {
+            this._model.remove({ _id: this.toObjectId(_id) }, (err) => callback(err, null));
+            return;
+        }
+        return new Promise((resolve: any, reject: any) => {
+            this._model.remove({ _id: this.toObjectId(_id) }, (err) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve();
+            });
+        });
     }
 
     /**
-     * 모든 객체 검색 
+     * 모든 객체 검색 (callback 객체가 없다면 Promise 라고 판단하고 대응한다.)
      * 
      * @param {(error: any, result: any) => void} callback
      */
-    findAll(callback: (error: any, result: any) => void) {
-        this._model.find({}, callback);
+    findAll(callback: (error: any, result: any) => void): Promise<Object> {
+        if (callback) {
+            this._model.find({}, callback);
+            return;
+        }
+        return new Promise((resolve: any, reject: any) => {
+            this._model.find({}, (err: any, res: mongoose.Document[]) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
     }
 
     /**
-     * 검색
+     * 검색 (callback 객체가 없다면 Promise 라고 판단하고 대응한다.)
      * 
      * @param {string} _id
      * @param {(error: any, result: T) => void} callback
      */
-    findById(_id: string, callback: (error: any, result: T) => void) {
-        this._model.findById(_id, callback);
+    findById(_id: string, callback: (error: any, result: T) => void): Promise<Object> {
+        if (callback) {
+            this._model.findById(_id, callback);
+            return;
+        }
+        return new Promise((resolve: any, reject: any) => {
+            this._model.findById(_id, (err: any, res: mongoose.Document) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(res);
+            });
+        });
+
     }
 
     /**
@@ -70,6 +129,6 @@ export default class CommonBusiness<T extends mongoose.Document> {
      * @returns {mongoose.Types.ObjectId}
      */
     private toObjectId(_id: string): mongoose.Types.ObjectId {
-        return mongoose.Types.ObjectId.createFromHexString(_id)
+        return mongoose.Types.ObjectId.createFromHexString(_id);
     }
 }
