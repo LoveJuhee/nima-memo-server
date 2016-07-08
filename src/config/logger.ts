c14dd3845bc9a2a1cdbafc05b448092ad30d30d3
hyounwoo.ko <lj1212.kit@gmail.com>
2016-07-08 19:40:19 +0900

    logger 추가

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -2,8 +2,10 @@
 export const LOGGING_API_SERVER: string = 'api:server';
 export const LOGGING_PROVIDER_UTIL_REQUEST: string = 'provider:util:request';
 export const LOGGING_MONGO_MANAGER: string = 'app:db:manager';
+export const LOGGING_BUSINESS_COMMON: string = 'business:common';
 
 /* test-code */
 /** TDD */
-export const LOGGING_TDD_MONGODB: string = 'tdd:mongodb';
+export const LOGGING_TDD_PRESET_MONGO_MANAGER: string = 'tdd:preset:mongo-manager';
+export const LOGGING_TDD_BUSINESS_USER: string = 'tdd:business:user';
 /* end-test-code */
