5ee330a1d1b83d85ab930da3be23123d6321f1a5
hyounwoo.ko <lj1212.kit@gmail.com>
2016-07-08 09:46:22 +0900

    user 모델 정리

--- src/model/user/user.ts
+++ src/model/user/user.ts
@@ -6,11 +6,5 @@ import {IUserModel, UserSchema} from './user-schema';
 /**
  * 모델 생성
  */
-let User: mongoose.Model<IUserModel>;
-try {
-    User = mongoose.model<IUserModel>('User');
-} catch (err) {
-    User = mongoose.model<IUserModel>('User', UserSchema);
-}
-
+let User: mongoose.Model<IUserModel> = mongoose.model<IUserModel>('User', UserSchema);
 export default User;
