97deb5523e57d7416e202139e7a75d4c0d3ee0c1
hyounwoo.ko <lj1212.kit@gmail.com>
2016-07-10 17:08:36 +0900

    factory 내용 수정

--- src/model/account/account.ts
+++ src/model/account/account.ts
@@ -6,5 +6,6 @@ import {IAccountModel, AccountSchema} from './account-schema';
 /**
  * 팩토리 생성
  */
-let AccountFactory: mongoose.Model<IAccountModel> = mongoose.model<IAccountModel>('Account', AccountSchema);
+let AccountFactory: mongoose.Model<IAccountModel>;
+AccountFactory = mongoose.model<IAccountModel>('Account', AccountSchema);
 export default AccountFactory;
