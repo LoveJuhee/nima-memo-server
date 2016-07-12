5bebd008c38031b05f2eaa32a2cd37a06307653f
hyounwoo.ko <lj1212.kit@gmail.com>
2016-07-13 00:12:28 +0900

    저장 시 암호화 처리

--- src/model/account/account-schema.ts
+++ src/model/account/account-schema.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import * as mongoose  from 'mongoose';
+import {Schema, Document, HookNextFunction}  from 'mongoose';
 import * as bcrypt from 'bcrypt-nodejs';
 
 export interface IAccount {
@@ -10,11 +10,43 @@ export interface IAccount {
     updated_at: Date;
 };
 
-export interface IAccountModel extends IAccount, mongoose.Document { };
+export interface IAccountModel extends IAccount, Document { };
 
-export let AccountSchema = new mongoose.Schema({
+export let AccountSchema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     created_at: Date,
     updated_at: Date
 });
+
+// 저장 이전에 할 동작
+AccountSchema.pre('save', hashPassword);
+
+// 업데이트 이전에 할 동작
+AccountSchema.pre('findOneAndUpdate', function hashPassword(next: HookNextFunction) {
+    console.log(this._update);
+    let user = this._update;
+    if (!user.newPassword) {
+        delete user.password;
+        return next();
+    } else {
+        user.password = bcrypt.hashSync(user.newPassword);
+        return next();
+    }
+});
+
+/**
+ * hash 생성
+ * 
+ * @param {HookNextFunction} next
+ * @returns
+ */
+function hashPassword(next: HookNextFunction) {
+    var user = this;
+    if (!user.isModified('password')) {
+        return next();
+    } else {
+        user.password = bcrypt.hashSync(user.password);
+        return next();
+    }
+}
