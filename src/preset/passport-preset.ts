87baaf2c68ce89121764a78dc905634fc96c9728
hyounwoo.ko <lj1212.kit@gmail.com>
2016-07-13 13:44:34 +0900

    passport-local 구현 중

--- src/preset/passport-preset.ts
+++ src/preset/passport-preset.ts
@@ -5,11 +5,14 @@
 import {Passport} from 'passport';
 import {Strategy as LocalStrategy, IStrategyOptionsWithRequest} from 'passport-local';
 
-import {IUserModel} from '../model/user/user-schema';
-import UserFactory from '../model/user/user';
-import UserBusiness from '../business/user/user-business';
+import {IAccountModel} from '../model/account/account-schema';
+import factory from '../business/account/account-business';
 
 import passportUtil from '../util/passport-util';
+
+import {LOGGING_PRESET_PASSPORT} from '../config/logger';
+import * as debugClass from 'debug';
+let debug: debug.IDebugger = debugClass(LOGGING_PRESET_PASSPORT);
 
 export function setupStrategies(passport: Passport): void {
 
@@ -20,13 +23,17 @@ export function setupStrategies(passport: Passport): void {
     // passport needs ability to serialize and unserialize users out of session
 
     // used to serialize the user for the session
-    passport.serializeUser(function (user: any, done: any) {
+    passport.serializeUser(function (user: IAccountModel, done: any) {
+        debug(`passport.serializeUser`);
+        debug(user);
         done(null, user.id);
     });
 
     // used to deserialize the user
     passport.deserializeUser(function (id: any, done: any) {
-        UserFactory.findById(id, function (err: any, user: IUserModel) {
+        debug(`passport.deserializeUser`);
+        factory.findById(id, (err, user) => {
+            debug(user);
             done(err, user);
         });
     });
@@ -42,52 +49,50 @@ export function setupStrategies(passport: Passport): void {
     // ============ //
     passport.use('local-signup',
         new LocalStrategy(strategyParams, (req, email, password, done) => {
+            debug(`passport.local-signup`);
+            debug(`${email}, ${password}`);
+
             // async
             process.nextTick(function () {
+                let email: string;
+                let password: string;
+
+                // req.user 가 있으면 req.user 값으로 계정을 만든다. 없으면 body에서 추출한다. 
                 if (!req.user) {
-                    // first we try to find the user to see if already exists
-                    UserFactory.findOne({ 'email': email }, function (err: any, user: IUserModel) {
-                        if (err) {
-                            return done(err);
-                        }
-
-                        // 검색결과가 있다면 등록된 email이라고 반환을 한다.
-                        if (user) {
-                            return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-                        } else {
-                            // email, password를 제외한 데이터는 모두 body 객체에서 정보를 가져와서 생성한다.
-                            let body = req.body;
-                            let newUser: Object = {
-                                name: body.name,
-                                email: email,
-                                password: passportUtil.generateHash(password),
-                            };
-
-                            // 계정정보를 이용하여 IUserModel을 생성한다.
-                            UserFactory.create(newUser, (err: any, res: IUserModel) => {
-                                // 생성된 IUserModel을 추가한다.
-                                new UserBusiness().create(res)
-                                    .then(() => { return done(null, newUser); })
-                                    .catch(err => { return done(err); });
-                            });
-                        }
-                    });
+                    email = req.body.param.email;
+                    password = req.body.param.password;
                 } else {
-                    // If the user is already logged in, we add the credential into profile
-                    var user = req.user;
-
-                    // set the credentials
-                    user.email = email;
-                    user.password = passportUtil.generateHash(password);
-
-                    // 계정정보를 이용하여 IUserModel을 생성한다.
-                    UserFactory.create(user, (err: any, res: IUserModel) => {
-                        // 생성된 IUserModel을 추가한다.
-                        new UserBusiness().create(res)
-                            .then(() => { return done(null, user); })
-                            .catch(err => { return done(err); });
-                    });
+                    email = req.user.email;
+                    password = req.user.password;
                 }
+
+                // email 계정이 있는지 확인하고 없으면 추가하는 흐름으로 구현한다.
+                factory.findByEmail(email, (err, account) => {
+                    if (err) {
+                        debug(`passport.local-signup findByEmail err`);
+                        debug(err);
+                        return done(err);
+                    }
+
+                    // 검색결과가 있으면 이미 가입한 계정으로 안내한다.
+                    if (account) {
+                        return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                    }
+
+                    factory.create({ email, password })
+                        // 생성 성공에 따른 리턴 처리
+                        .then(res => {
+                            debug(`passport.local-signup create succeed`);
+                            debug(res);
+                            return done(null, res);
+                        })
+                        // 생성 실패에 따른 리턴처리
+                        .catch(err => {
+                            debug(`passport.local-signup create failed`);
+                            debug(err);
+                            return done(err);
+                        });
+                });
             });
         })
     );
@@ -98,21 +103,22 @@ export function setupStrategies(passport: Passport): void {
     // We create another strategy for the login process
     passport.use('local-login',
         new LocalStrategy(strategyParams, (req, email, password, done) => {
+            debug(`passport.local-login`);
+            debug(`${email}, ${password}`);
+
             // first check if the user already exists
-            UserFactory.findOne({ 'email': email }, function (err: any, user: IUserModel) {
+            factory.findOne({ email, password }, (err, user) => {
                 // If there are any error, return the error
                 if (err) {
+                    debug(`passport.local-login errored`);
+                    debug(err);
                     return done(err);
                 }
 
-                // if no user is found, return message
+                // 반환이 null 이면 email, password 가 다르다고 판단한다.
                 if (!user) {
-                    return done(null, false, req.flash('loginMessage', 'No user found.'));
-                }
-
-                // if the user exists, we check the password
-                if (!passportUtil.isValidPassword(password, user.password + '')) {
-                    return done(null, false, req.flash('loginMessage', 'Opps! Wrong password.'));
+                    debug(`passport.local-login findOne failed`);
+                    return done(null, false, req.flash('loginMessage', 'login failed.'));
                 }
 
                 // if everything is ok, return the user
