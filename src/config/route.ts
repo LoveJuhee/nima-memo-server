0944cb8977f369fb36b6d90fe7ffa02457e7312e
hyounwoo.ko <lj1212.kit@gmail.com>
2016-07-15 01:10:43 +0900

    route uri 변수명 속성 지정

--- src/config/route.ts
+++ src/config/route.ts
@@ -1,4 +1,4 @@
 /** REST ROUTE 관련 기본 설정 */
-export const ROUTE_URI_ACCOUNTS = '/';
-export const ROUTE_URI_SERVERS = '/api/servers';
-export const ROUTE_URI_USERS = '/api/users';
+export const ROUTE_URI_ACCOUNTS: string = '/';
+export const ROUTE_URI_SERVERS: string = '/api/servers';
+export const ROUTE_URI_USERS: string = '/api/users';
