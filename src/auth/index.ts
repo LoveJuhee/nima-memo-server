70d59e393a622727122fc81b313c9c32942dc05b
hyounwoo.ko <lj1212.kit@gmail.com>
2016-07-21 20:03:36 +0900

    local.index 클래스화 처리에 따른 수정

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,18 +2,20 @@
 
 import * as express from 'express';
 import * as passport from 'passport';
+
 import config from '../config/environment';
-import User from '../api/user/user.model';
+import User from '../api/user/user.business';
+
+import {AuthLocalIndex} from './local';
 
 // Passport Configuration
-require('./local/passport').setup(User, config);
 require('./facebook/passport').setup(User, config);
 require('./google/passport').setup(User, config);
 require('./twitter/passport').setup(User, config);
 
 var router = express.Router();
 
-router.use('/local', require('./local'));
+router.use('/local', new AuthLocalIndex(User, config).routes);
 router.use('/facebook', require('./facebook'));
 router.use('/twitter', require('./twitter'));
 router.use('/google', require('./google'));
--- src/auth/local/index.ts
+++ src/auth/local/index.ts
@@ -6,21 +6,44 @@ import * as passport from 'passport';
 import {signToken} from '../auth.service';
 import {IUserModel} from '../../api/user/user.model';
 
+import {LocalPassport} from './passport';
+
 var router = Router();
 
-router.post('/', function (req: Request, res: Response, next: NextFunction) {
-    passport.authenticate('local', function (err: any, user: IUserModel, info: any) {
-        var error = err || info;
-        if (error) {
-            return res.status(401).json(error);
-        }
-        if (!user) {
-            return res.status(404).json({ message: 'Something went wrong, please try again.' });
-        }
+/**
+ * 컨트롤러 연결 클래스
+ * @class
+ */
+export class AuthLocalIndex {
+    constructor(factory: any, config: any) {
 
-        var token = signToken(user._id, user.role + '');
-        res.json({ token });
-    })(req, res, next);
-});
+    }
+    /**
+     * route 처리 객체 반환
+     *
+     * @readonly
+     * @type {Router}
+     */
+    get routes(): Router {
+        router.post('/', (req, res, next) => {
+            passport.authenticate('local', function (err: any, user: IUserModel, info: any) {
+                var error = err || info;
+                if (error) {
+                    return res.status(401).json(error);
+                }
+                if (!user) {
+                    return res.status(404).json({ message: 'Something went wrong, please try again.' });
+                }
 
-export default router;
+                var token = signToken(user._id, user.role);
+                res.json({ token });
+            })(req, res, next);
+        });
+
+        return router;
+    }
+
+    toString() {
+        return 'AuthLocalIndex class';
+    }
+}
