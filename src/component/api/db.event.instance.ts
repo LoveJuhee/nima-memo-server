8ee142505f24bc805f4134a27323420d679722a1
hyounwoo.ko <lj1212.kit@gmail.com>
2016-07-28 14:10:30 +0900

    DbEvent 관련 클래스 생성 및 수정

--- src/component/api/db.event.instance.ts
+++ src/component/api/db.event.instance.ts
@@ -1,7 +1,7 @@
 'use strict';
 
-export const DB_EVENT_SAVE = { dbEvent: 'save', event: 'save' };
-export const DB_EVENT_UPDATE = { dbEvent: 'update', event: 'update' };
-export const DB_EVENT_REMOVE = { dbEvent: 'remove', event: 'remove' };
-export const DB_EVENT_FIND_BY_ID_AND_UPDATE = { dbEvent: 'findByIdAndUpdate', event: 'update' };
-export const DB_EVENT_FIND_BY_ID_AND_REMOVE = { dbEvent: 'findByIdAndRemove', event: 'remove' };
+import {DbEventKeyValue} from './db.event';
+
+export const DB_EVENT_SAVE = new DbEventKeyValue('save', 'save');
+export const DB_EVENT_UPDATE = new DbEventKeyValue('update', 'update');
+export const DB_EVENT_REMOVE = new DbEventKeyValue('remove', 'remove');
