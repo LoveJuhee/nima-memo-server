534481abee7b2ede8b92f5af6e24987effc97677
hyounwoo.ko <lj1212.kit@gmail.com>
2016-08-08 01:28:42 +0900

    Memo 추가에 따른 수정 - logger : debug 정보 추가 - route : route 정보 추가 - express-preset : route 경로 추가

--- src/config/route.ts
+++ src/config/route.ts
@@ -1,5 +1,6 @@
 /** REST ROUTE 관련 기본 설정 */
 export const ROUTE_URI_ACCOUNTS: string = '/';
 export const ROUTE_URI_SERVERS: string = '/api/servers';
+export const ROUTE_URI_MEMOS: string = '/api/memos';
 export const ROUTE_URI_USERS: string = '/api/users';
 export const ROUTE_URI_AUTH: string = '/auth';
